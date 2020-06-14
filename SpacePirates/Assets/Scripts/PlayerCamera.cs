527b22d4929423b6043263441e188c3f9b8bb442
Jan <Baerengames@gmail.com>
2020-06-14 10:05:05 +0200

    Added Camera Movment

---
+++ SpacePirates/Assets/Scripts/PlayerCamera.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class PlayerCamera : MonoBehaviour
+{
+    // Controlls the camera movements in respect to the player position
+    public Transform player;
+    public Vector3 offset;
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        transform.position = player.position + offset;
+    }
+}
