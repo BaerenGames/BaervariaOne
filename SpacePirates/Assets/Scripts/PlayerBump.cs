9ea40a71a2f9edca57e973766416aa19eebd398e
Jan <Baerengames@gmail.com>
2020-06-15 22:09:31 +0200

    Added Player Knockback
    Implemented a knockback system that pushes the player back when they hit an obstacles, as well as disabling their controlls for a certain period of time.

---
+++ SpacePirates/Assets/Scripts/PlayerBump.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class PlayerBump : MonoBehaviour
+{
+    public PlayerMovement movement;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.tag == "Obstacle")
+        {
+            movement.Bump();
+        }
+    }
+}
--- SpacePirates/Assets/Scripts/PlayerMovement.cs
+++ SpacePirates/Assets/Scripts/PlayerMovement.cs
@@ -1,9 +1,11 @@
 ﻿using UnityEditor.Timeline;
+using System.Collections;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
     public Rigidbody player;
+    public bool movement_enabled;
     public float max_speed;
     public float min_speed;
     public float min_break_speed;
@@ -21,6 +23,8 @@ public class PlayerMovement : MonoBehaviour
     // Use this for initialization
     private void Start()
     {
+        // Asks if the Movement itself is enabled
+        movement_enabled = true;
         // Describes the maximal speed for the player
         max_speed = 40f;
         // Describes the minimal speed for the player
@@ -53,90 +57,122 @@ public class PlayerMovement : MonoBehaviour
     void FixedUpdate()
     {
         bool breaking = false;
-
-        // Manage Decay
-        if (current_speed > min_speed)
+        if (movement_enabled == true)
         {
-            current_speed -= speed_decay * Time.deltaTime;
-            if (current_speed < min_speed)
+            // Manage Decay
+            if (current_speed > min_speed)
             {
-                current_speed = min_speed;
+                current_speed -= speed_decay * Time.deltaTime;
+                if (current_speed < min_speed)
+                {
+                    current_speed = min_speed;
+                }
             }
-        }
 
-        if (acceleration_modifier > 0)
-        {
-            acceleration_modifier -= acceleration_decay * Time.deltaTime;
-            if (acceleration_modifier < 0)
-            {
-                acceleration_modifier = 0;
-            }
-        }
-        else if (acceleration_modifier < 0)
-        {
-            acceleration_modifier += acceleration_decay * Time.deltaTime;
             if (acceleration_modifier > 0)
             {
-                acceleration_modifier = 0;
-            }
-        }
-
-        // Manipulate Acceleration
-        if (Input.GetKey("w"))
-        {
-            if (acceleration_modifier < acceleration_modifier_max)
-            {
-                acceleration_modifier += acceleration_modifier_rate * Time.deltaTime;
-                if (acceleration_modifier > acceleration_modifier_max)
+                acceleration_modifier -= acceleration_decay * Time.deltaTime;
+                if (acceleration_modifier < 0)
                 {
-                    acceleration_modifier = acceleration_modifier_max;
+                    acceleration_modifier = 0;
                 }
             }
-        }
-
-        if (Input.GetKey("s"))
-        {
-            breaking = true;
-            if (acceleration_modifier > acceleration_modifier_min)
+            else if (acceleration_modifier < 0)
             {
-                acceleration_modifier -= acceleration_modifier_rate * Time.deltaTime;
-                if (acceleration_modifier < acceleration_modifier_min)
+                acceleration_modifier += acceleration_decay * Time.deltaTime;
+                if (acceleration_modifier > 0)
                 {
-                    acceleration_modifier = acceleration_modifier_min;
+                    acceleration_modifier = 0;
                 }
             }
-        }
 
-        // Manipulate Orientation
-        if (Input.GetKey("a"))
-        {
-            player.transform.Rotate(0, -rotation_rate * Time.deltaTime, 0);
-        }
-
-        if (Input.GetKey("d"))
-        {
-            player.transform.Rotate(0, rotation_rate * Time.deltaTime, 0);
-        }
-
-        // Set the current speed
-        current_speed += base_acceleration_rate * acceleration_modifier * Time.deltaTime;
-        if (current_speed > max_speed)
-        {
-            current_speed = max_speed;
-        }
-        else if (current_speed < min_speed)
-        {
-            if (breaking == false)
+            // Manipulate Acceleration
+            if (Input.GetKey("w"))
             {
-                current_speed = min_speed;
+                if (acceleration_modifier < acceleration_modifier_max)
+                {
+                    acceleration_modifier += acceleration_modifier_rate * Time.deltaTime;
+                    if (acceleration_modifier > acceleration_modifier_max)
+                    {
+                        acceleration_modifier = acceleration_modifier_max;
+                    }
+                }
             }
-            else
-            {
-                current_speed = min_break_speed;
-            }
-        }
 
-        // Move the player
-        player.transform.position += transform.forward * Time.deltaTime * current_speed;
+            if (Input.GetKey("s"))
+            {
+                breaking = true;
+                if (acceleration_modifier > acceleration_modifier_min)
+                {
+                    acceleration_modifier -= acceleration_modifier_rate * Time.deltaTime;
+                    if (acceleration_modifier < acceleration_modifier_min)
+                    {
+                        acceleration_modifier = acceleration_modifier_min;
+                    }
+                }
+            }
+
+            // Manipulate Orientation
+            if (Input.GetKey("a"))
+            {
+                player.transform.Rotate(0, -rotation_rate * Time.deltaTime, 0);
+            }
+
+            if (Input.GetKey("d"))
+            {
+                player.transform.Rotate(0, rotation_rate * Time.deltaTime, 0);
+            }
+
+            // Set the current speed
+            current_speed += base_acceleration_rate * acceleration_modifier * Time.deltaTime;
+            if (current_speed > max_speed)
+            {
+                current_speed = max_speed;
+            }
+            else if (current_speed < min_speed)
+            {
+                if (breaking == false)
+                {
+                    current_speed = min_speed;
+                }
+                else
+                {
+                    current_speed = min_break_speed;
+                }
+            }
+
+            // Move the player
+            player.transform.position += transform.forward * Time.deltaTime * current_speed;
+        }
+    }
+
+    // Handles the situation in which the player collides with an obstacles
+    public void Bump()
+    {
+        // Disable the movement for the player
+        movement_enabled = false;
+
+        // Give the player backwards push, depending on their speed at the time of the collision
+        float bump_multiplier = 1.5f;
+        float bump_distance = (50f + (current_speed / 1.5f)) * bump_multiplier;
+        player.AddForce((-1 * Vector3.forward) * bump_distance);
+
+        // Set the players speed, acceleration and acceleration modifier to zero
+        current_speed = 0f;
+        current_acceleration = 0f;
+        acceleration_modifier = 0f;
+
+        // Call coroutine that waits for a period of time (depending on speed) and reactive movement
+        float wait_shock = 0.75f + (current_speed * 0.075f);
+        StartCoroutine(ReactivateMovement(wait_shock));
+    }
+
+    IEnumerator ReactivateMovement(float time)
+    {
+        // Coroutine that reactivates movement and removes pushback after given time period
+        yield return new WaitForSeconds(time);
+        movement_enabled = true;
+        player.velocity = Vector3.zero;
+        player.angularVelocity = Vector3.zero;
     }
 }
