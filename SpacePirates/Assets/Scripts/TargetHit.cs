185b8d574af602b688e1184b518da716066a3f1b
Jan <Baerengames@gmail.com>
2020-06-17 21:58:35 +0200

    Added Explosions

--- SpacePirates/Assets/Scripts/TargetHit.cs
+++ SpacePirates/Assets/Scripts/TargetHit.cs
@@ -5,12 +5,22 @@ using UnityEngine;
 public class TargetHit : MonoBehaviour
 {
     public GameObject thisRocket;
+    public Rigidbody thisRocketBody;
+    public GameObject explosion;
 
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.collider.tag == "Obstacle")
         {
+            Object.Instantiate(explosion, thisRocketBody.position, thisRocketBody.rotation);
             Object.Destroy(thisRocket);
+            AwaitAnimation(2.51f);
         }
+    }
+
+    IEnumerator AwaitAnimation(float time)
+    {
+        yield return new WaitForSeconds(time);
+        Object.Destroy(explosion);
     }
 }
